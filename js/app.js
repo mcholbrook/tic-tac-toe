55147da82378035066517fd95e798cec951d9fe3
mcholbrook <mirandacholbrook@gmail.com>
2020-11-14 11:34:18 -0500

    Add cached element references

--- js/app.js
+++ js/app.js
@@ -8,14 +8,7 @@ let colors = {
   '-1': 'blue'
  }
 
- let win1 = [sq1, sq2, sq3]
- let win2 = [sq4, sq5, sq6]
- let win3 = [sq7, sq8, sq9]
- let win4 = [sq1, sq4, sq7]
- let win5 = [sq2, sq5, sq8]
- let win6 = [sq3, sq6, sq9]
- let win7 = [sq3, sq5, sq7]
- let win8 = [sq1, sq5, sq9]
+ let winCombos = [[sq0, sq1, sq2], [sq3, sq4, sq5], [sq6, sq7, sq8], [sq0, sq3, sq6], [sq1, sq4, sq7], [sq2, sq5, sq8], [sq0, sq4, sq8], [sq2, sq4, sq6]]
 
 
 /*---------------------------- Variables (state) ----------------------------*/
@@ -37,15 +30,17 @@ const sq5 = document.getElementById('sq6')
 const sq6 = document.getElementById('sq7')
 const sq7 = document.getElementById('sq8')
 const sq8 = document.getElementById('sq9')
+const resetButton = document.getElementById('resetButton')
+const message = document.getElementById('message')
 
 /*----------------------------- Event Listeners -----------------------------*/
 
 // This is where you should put the event listener
 // for a mouse-click
 document.body.addEventListener('click', (e) =>{
-  e.target.id ===
-  
+  e.target.id
 })
+resetButton.addEventListener('click', init)
 
 /*-------------------------------- Functions --------------------------------*/
 
@@ -61,14 +56,14 @@ function init(){
   turn = 1
   winner = null
   board = [null, null, null, null, null, null, null, null, null]
-
+  render()
 }
 
 // On-Click function:
 // Set up what happens when one of the elements
 // is clicked
 function onClick(){
-
+  if (sq){}
 }
 
 // Check winner function:
@@ -84,5 +79,9 @@ function isWinner(){
 // on the page, updating the elements to reflect
 // either X or O depending on whose turn it is
 function render(){
-
+  board.forEach(function(square, idx){
+    console.log(square, idx)
+  })
 }
+
+render()
