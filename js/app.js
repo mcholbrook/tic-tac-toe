f3fb79a56671262cc17c719536a3d9ce0baffd47
mcholbrook <mirandacholbrook@gmail.com>
2020-11-15 15:34:44 -0500

    Fix win logic issue

--- js/app.js
+++ js/app.js
@@ -1,8 +1,5 @@
 /*-------------------------------- Constants --------------------------------*/
-//Player 1 = red, X, 1
-//Player 2 = blue, O, -1
-
-let colors = {
+let playerText = {
   'null': '',
   '1': 'X',
   '-1': 'O'
@@ -14,9 +11,9 @@ let colors = {
 /*---------------------------- Variables (state) ----------------------------*/
 
 // Variables might include (board/turn/winner)
-let turn = null;
+let turn = null
 let board = []
-let winner = null;
+let winner = null
 
 /*------------------------ Cached Element References ------------------------*/
 
@@ -35,14 +32,6 @@ resetButton.addEventListener('click', init)
 
 /*-------------------------------- Functions --------------------------------*/
 
-
-// Some functions you might choose to use:
-
-// Initialization function:
-// Where you set your initial state, setting up 
-// what the board will look like upon loading
-
-
 function init(){
   turn = 1
   winner = null
@@ -50,13 +39,10 @@ function init(){
   render()
 }
 
-// On-Click function:
-// Set up what happens when one of the elements
-// is clicked
 function onClick(e){
   let currentSquare = e.target.id
   console.log(currentSquare)
-  if (board[Number(currentSquare)]){
+  if (board[Number(currentSquare)] || winner){
     return
   } 
   else {
@@ -69,16 +55,7 @@ function onClick(e){
   }
 }
 
-// Check winner function:
-// Checks the current state of the board for
-// a winner and changes the state of the winner
-// variable if so
 function isWinner(){
-    //  if (board[0] && board[0] === board[1] && board[0] === board[2]){
-    //   winner = board[0]
-    //   console.log(winner)
-    //   return winner}
-
   winPossibilities.forEach(function(possibility){
     if (board[possibility[0]] && board[possibility[0]] === board[possibility[1]] && board[possibility[0]] === board[possibility[2]]){
       winner = board[possibility[0]]
@@ -89,32 +66,21 @@ function isWinner(){
        console.log(`This is a tie game. ${winner}`)
        return winner
      }
-     
-     
-    //  else if (board.includes(null)){
-    //   winner = null
-    //   console.log(`There's no winner yet: ${winner}`)
-    //   return winner
-    // } winner = 'T'
-    // return winner 
  })
 }
 
-
-// Render function:
-// Displays the current state of the board
-// on the page, updating the elements to reflect
-// either X or O depending on whose turn it is
 function render(){
   board.forEach(function(square, idx){
-    squares[idx].innerText = colors[square]
+    squares[idx].innerText = playerText[square]
   })
   console.log(winner)
   if (winner === "T"){
     message.innerHTML = "It's a tie!"
-  } else if (winner === 1 || winner === -1) {
-    message.innerHTML = `${winner} wins this round!`
-   } else {
+  } else if (winner === 1) {
+    message.innerHTML = `Player one wins this round!`
+  } else if (winner === -1){
+     message.innerHTML = `Player two wins this round!`
+  } else {
      if (turn === 1){
       message.innerHTML = `It's player one's turn!`
      } else {
